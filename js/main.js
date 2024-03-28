904231ec1036b2f81f7e9d840ea5e5b51f0af735
CC8IV <CROWNEDX.CREATIV@GMAIL.COM>
2024-03-28 10:56:12 +0100

    update 28/3/2024

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+// ============= navbar functionality ================//
+let hamburgerEl = document.querySelector("div.hamburger");
+let menuEl = document.querySelector("ul#menu-list");
+
+dispnone = () =>{
+    menuEl.style.display = "none";
+}
+
+hamburgerEl.addEventListener("click", () => {
+    menuEl.classList.toggle("active");
+    
+    if(menuEl.classList.value == "active"){
+        menuEl.style.display = "flex";
+        menuEl.style.animation = "drop .2s ease forwards";
+
+        hamburgerEl.innerHTML = "&times;";
+        console.log("active");
+    }
+    else{
+        menuEl.style.animation = "takeup .2s ease forwards";
+        
+        hamburgerEl.innerHTML = "&checkmark;";
+        setTimeout(dispnone, 200);
+    }
+    // console.log(menuEl);
+    // console.log(menuEl.classList.value);
+    // console.log(menuEl)
+})
+
+// ============= carousel functionality ================//
 let slider = document.querySelector(".slider");
 let item = document.querySelectorAll(".slider span");
 let container = document.querySelector(".position-list");
@@ -11,16 +41,36 @@ function moveSlider(index){
         slideIndex = item.length - 1;
     } else {
         slideIndex = index;
-    }
+    }    
 
     const translateValue = - slideIndex * 46 + 'px';
     slider.style.transform = 'translateY(' + translateValue + ')';
-}
+}    
 
 activate = () => {
 
     moveSlider(slideIndex + 1);
-}
+}    
 
 
 setInterval(activate, 2000);
+
+
+// ============= button functionality ================//
+let hireMeBtn = document.querySelectorAll("button.hire-me");
+let downloadBtn = document.querySelectorAll("button.download");
+
+//hire-me functionality
+hireMeBtn.forEach(e => {
+    e.addEventListener("click", () => {
+            console.log("hire-me");
+    });
+})
+
+//hire-me functionality
+downloadBtn.forEach(e => {
+    e.addEventListener("click", () => {
+            console.log("download");
+    });
+})
+
